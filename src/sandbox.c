419343e0b83fff43939e9b74e97fb63c47eea1fb
Ivan <ivanbabaev217@gmail.com>
2026-09-16 15:45:38 +0800

    Test layer is now pushed onto the layer stack on sandbox initialization

--- src/sandbox.c
+++ src/sandbox.c
@@ -1,5 +1,7 @@
 #include "sandbox.h"
 
+#include "test_layer.h"
+
 bool sandbox_init(application_t *self)
 {
     if (!application_init(self))
@@ -7,6 +9,11 @@ bool sandbox_init(application_t *self)
 
     window_set_title(&self->main_window, "Sandbox");
     window_set_size(&self->main_window, 800, 600);
+
+    layer_t test_layer;
+    test_layer_init(&test_layer);
+
+    application_push_layer(self, test_layer);
 
     return true;
 }
