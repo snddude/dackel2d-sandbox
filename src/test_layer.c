ae64267d8589cdfbed7d02c4ac393e40aae29f88
Ivan <ivanbabaev217@gmail.com>
2026-09-16 15:45:08 +0800

    Create test_layer.c

---
+++ src/test_layer.c
@@ -0,0 +1,40 @@
+#include "test_layer.h"
+
+static log_level_t dynamic_log_level = LogLevel_Error;
+
+static void process_event(event_t *event)
+{
+    if (event->type == EventType_Key && event->key.pressed && !event->key.echo)
+        switch (event->key.code)
+        {
+            case KeyCode_0:
+                if (dynamic_log_level < LogLevel_Info)
+                    dynamic_log_level++;
+                else
+                {
+                    log_warning("Set log level to None");
+                    dynamic_log_level = LogLevel_None;
+                }
+
+                set_log_level(dynamic_log_level);
+                log_warning("Incremented log level to: %d", dynamic_log_level);
+                break;
+            case KeyCode_1:
+                log_info("This is an informational message!");    
+                break;
+            case KeyCode_2:
+                log_error("This is an error!");
+                break;
+            case KeyCode_3:
+                log_warning("This is an warning!");
+                break;
+            default:
+                break;
+        }
+}
+
+void test_layer_init(layer_t *self)
+{
+    layer_init(self);
+    self->process_event_pfn = &process_event;
+}
